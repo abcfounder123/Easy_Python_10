0d88dd8b24300b92928ab3916d3092b1d77c7b14
abcfounder <156555912+abcfounder123@users.noreply.github.com>
2026-08-02 17:42:36 +0700

    Selection

---
+++ day_10_selection.py
@@ -0,0 +1,29 @@
+
+"""
+
+Selection
+
+1. Normal Statement ( ; end of line)
+   - motor on
+   - motor off
+   - pass
+   - fail
+
+2. Conditional Statement
+   - If water level is low, motor on.  
+   - If water level is high, motor off. 
+   - if exam pass, show "pass".
+   - if exam fail, show "fail".
+
+3. Conditional if Statement
+   - boolean data type
+   - True
+
+4. Conditional else Statement
+   - boolean data type
+   - False
+
+####################################################################################
+
+
+"""
